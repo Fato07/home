80102d0e0fa3f317161d4db8ffcda523f7e36cfe
fadosu <fadosu@ttu.ee>
2020-09-06 23:06:15 +0300

    Updates

---
+++ precache-manifest.4ae79311fa6ba29bb735e5319f0f2ccd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de0da6198ac7ac5f956b3d6976f60638",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "7426b5cd78dc30ae0638",
+    "url": "/home/static/css/2.5b44f0a0.chunk.css"
+  },
+  {
+    "revision": "383a680fe33fe45d6c66",
+    "url": "/home/static/css/main.9fc689ea.chunk.css"
+  },
+  {
+    "revision": "7426b5cd78dc30ae0638",
+    "url": "/home/static/js/2.11047eb6.chunk.js"
+  },
+  {
+    "revision": "80fa05aac9f80c71b5f99ecec6ead10d",
+    "url": "/home/static/js/2.11047eb6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "383a680fe33fe45d6c66",
+    "url": "/home/static/js/main.94f90a5d.chunk.js"
+  },
+  {
+    "revision": "c0578b487099793a068c",
+    "url": "/home/static/js/runtime-main.3c590061.js"
+  },
+  {
+    "revision": "e43743c139a1eb4d319da89d6975ad10",
+    "url": "/home/static/media/resume.e43743c1.pdf"
+  }
+]);
