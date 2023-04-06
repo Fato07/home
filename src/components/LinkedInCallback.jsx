7db75239b34e1f990d8ba173637e28060739df82
Sha256_Nulled <Fathindos.FD@gmail.com>
2023-04-06 23:35:00 +0300

    linkedin callback test

--- src/components/LinkedInCallback.jsx
+++ src/components/LinkedInCallback.jsx
@@ -1,20 +1,18 @@
 import React, { useEffect } from "react"
 
 const LinkedInCallback = ({ history }) => {
-  useEffect(() => {
-    const params = new URLSearchParams(window.location.search)
-    const code = params.get("code")
-    const state = params.get("state")
+  // useEffect(() => {
+  //   const params = new URLSearchParams(window.location.search)
 
-    if (!code || state !== "123456") {
-      console.error("Error in LinkedIn authorization")
-      history.push("/")
-      return
-    }
+  //   if (!code) {
+  //     console.error("Error in LinkedIn authorization")
+  //     history.push("/")
+  //     return
+  //   }
 
-    // Exchange the authorization code for an access token.
-    // You can call a function here to handle the token exchange, then store the access token and use it for LinkedIn API requests.
-  }, [history])
+  //   // Exchange the authorization code for an access token.
+  //   // You can call a function here to handle the token exchange, then store the access token and use it for LinkedIn API requests.
+  // }, [history])
 
   return (
     <div>
@@ -24,5 +22,3 @@ const LinkedInCallback = ({ history }) => {
 }
 
 export default LinkedInCallback
-
-// https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=78ywaqnshvzsdu&redirect_uri=https://fato07.github.io/home/linkedin-callback&state=123456&scope=r_liteprofile%20r_emailaddress%20w_member_social
